4afb49af8f229eba54ada2b8c6c3d268c730fff7
T. G. <6294097+tomgk@users.noreply.github.com>
2025-07-28 21:02:24 +0200

    add checks

--- optional.cpp
+++ optional.cpp
@@ -1,3 +1,10 @@
 #include "optional.h"
 
+#include "nonzero.h"
+#include "nonnullptr.h"
+
 static_assert(sizeof(int) == sizeof(Optional<ExcludeValue<int, -1>>));
+
+static_assert(sizeof(int) == sizeof(Optional<NonZero<int>>));
+
+static_assert(sizeof(int*) == sizeof(Optional<NonNullPtr<int>>));
