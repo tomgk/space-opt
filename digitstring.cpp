bc29c3927b87ccbe7fa171c8ea0ef8f152dc9e5a
T. G. <6294097+tomgk@users.noreply.github.com>
2025-07-29 20:11:15 +0200

    Update digitstring.cpp

--- digitstring.cpp
+++ digitstring.cpp
@@ -5,8 +5,10 @@
 DigitString::DigitString(const char *str)
 {
     size_t len = strlen(str);
+    m_values.resize(len+len%2);
     for(size_t i = 0; i < len; ++i)
     {
-
+        m_values.push_back(access(i, false));
+        m_values.push_back(access(i, true));
     }
 }
