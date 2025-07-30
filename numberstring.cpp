e08e604df99f363787d21558783b866ae4375fdb
T. G. <6294097+tomgk@users.noreply.github.com>
2025-07-30 21:03:50 +0200

    add more

--- numberstring.cpp
+++ numberstring.cpp
@@ -2,20 +2,52 @@
 
 #include<stdexcept>
 
+static const char ALPHA_START = 'A';
+static const char ALPHA_END   = 'Z';
+static int ALPHA_LEN = ALPHA_END - ALPHA_START;
+
+static std::invalid_argument raiseInvalid(const char *str, size_t index)
+{
+    char ch = str[index];
+    return std::invalid_argument(std::string()+"invalid: "+str+" - digit DEC"+std::to_string(int(ch)));;
+}
+
 template<size_t N>
 int NumberCharset<N>::parseDigit(const char *str, size_t index)
 {
     char ch = str[index];
-    if(ch < '0' || ch >= '9')
-        throw std::invalid_argument(std::string()+"invalid: "+str+" - digit DEC"+std::to_string(int(ch)));
+    if(N <= 10)
+    {
+        if(ch < '0' || ch > '9')
+            throw raiseInvalid(str, index);
 
-    return ch - '0';
+        return ch - '0';
+    }
+    else
+    {
+        if(ch >= '0' && ch <= '9')
+            return ch - '0';
+        else if(ch >= ALPHA_START && ch <= ALPHA_END)
+            return ch - ALPHA_START + 10;
+        else
+            throw raiseInvalid(str, index);
+    }
 }
 
 template<size_t N>
 char NumberCharset<N>::toDigit(int value)
 {
-    return value + '0';
+    if(N <= 10)
+        return value + '0';
+    else
+    {
+        if(value < 10)
+            return value + '0';
+        else if(value < 10+ALPHA_LEN)
+            return ALPHA_START + value;
+        else
+            throw std::invalid_argument("invalid");
+    }
 }
 
 template class NumberCharset<10>;
