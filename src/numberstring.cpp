54ab3b88b0e466786451e47ce5ff4f8bb52976f7
T. G. <6294097+tomgk@users.noreply.github.com>
2025-08-04 19:03:10 +0200

    Move NumberCharset methods to header file

--- src/numberstring.cpp
+++ src/numberstring.cpp
@@ -2,51 +2,7 @@
 
 #include<stdexcept>
 
-static const char ALPHA_START = 'A';
-static const char ALPHA_END   = 'Z';
-static int ALPHA_LEN = ALPHA_END - ALPHA_START;
-
-static std::invalid_argument raiseInvalid(char ch)
+std::invalid_argument raiseInvalid(char ch)
 {
     return std::invalid_argument(std::string("invalid: DEC"+std::to_string(int(ch))));
 }
-
-template<size_t N>
-int NumberCharset<N>::getCharValue(char ch)
-{
-    if(N <= 10)
-    {
-        if(ch < '0' || ch > '9')
-            throw raiseInvalid(ch);
-
-        return ch - '0';
-    }
-    else
-    {
-        if(ch >= '0' && ch <= '9')
-            return ch - '0';
-        else if(ch >= ALPHA_START && ch <= ALPHA_END)
-            return ch - ALPHA_START + 10;
-        else
-            throw raiseInvalid(ch);
-    }
-}
-
-template<size_t N>
-char NumberCharset<N>::toChar(int value)
-{
-    if(N <= 10)
-        return value + '0';
-    else
-    {
-        if(value < 10)
-            return value + '0';
-        else if(value >= 10 && value < 26)
-            return ALPHA_START + (value - 10);
-        else
-            throw std::invalid_argument("invalid: "+std::to_string(value));
-    }
-}
-
-template class NumberCharset<10>;
-template class NumberCharset<16>;
--- src/numberstring.h
+++ src/numberstring.h
@@ -3,13 +3,53 @@
 
 #include "nibblestring.h"
 
+std::invalid_argument raiseInvalid(char ch);
+
 template<size_t N>
 class NumberCharset
 {
+    static constexpr char ALPHA_START = 'A';
+    static constexpr char ALPHA_END   = 'Z';
+    static constexpr int ALPHA_LEN = ALPHA_END - ALPHA_START;
 public:
-    static int getCharValue(char ch);
-    static char toChar(int value);
+    static int getCharValue(char ch)
+    {
+        if(N <= 10)
+        {
+            if(ch < '0' || ch > '9')
+                throw raiseInvalid(ch);
+
+            return ch - '0';
+        }
+        else
+        {
+            if(ch >= '0' && ch <= '9')
+                return ch - '0';
+            else if(ch >= ALPHA_START && ch <= ALPHA_END)
+                return ch - ALPHA_START + 10;
+            else
+                throw raiseInvalid(ch);
+        }
+    }
+
+    static char toChar(int value)
+    {
+        if(N <= 10)
+            return value + '0';
+        else
+        {
+            if(value < 10)
+                return value + '0';
+            else if(value >= 10 && value < 26)
+                return ALPHA_START + (value - 10);
+            else
+                throw std::invalid_argument("invalid: "+std::to_string(value));
+        }
+    }
 };
+
+template class NumberCharset<10>;
+template class NumberCharset<16>;
 
 using DecimalString = NibbleString<NumberCharset<10>>;
 using HexString = NibbleString<NumberCharset<16>>;
