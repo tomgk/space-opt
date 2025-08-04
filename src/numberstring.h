dc2f268dd3e93c12a996313ccfd3a0d0dc1c9dc8
T. G. <6294097+tomgk@users.noreply.github.com>
2025-08-04 19:13:47 +0200

    use more constants in DigitCharset

--- src/numberstring.h
+++ src/numberstring.h
@@ -8,25 +8,34 @@ std::invalid_argument raiseInvalid(char ch);
 template<size_t N>
 class DigitCharset
 {
+    static constexpr char NUM_START = '0';
+    static constexpr int NUM_COUNT = 10;
+
     static constexpr char ALPHA_START = 'A';
-    static constexpr char ALPHA_END   = 'Z';
-    static constexpr int ALPHA_LEN = ALPHA_END - ALPHA_START;
+    static constexpr int ALPHA_COUNT = 10;
+
+    static_assert(N >= 2);
+    static_assert(N <= NUM_COUNT + ALPHA_COUNT);
 public:
     static int getCharValue(char ch)
     {
-        if(N <= 10)
+        if constexpr(N <= NUM_COUNT)
         {
-            if(ch < '0' || ch > '9')
+            char numMax = NUM_START+N;
+
+            if(ch < NUM_START || ch > numMax)
                 throw raiseInvalid(ch);
 
-            return ch - '0';
+            return ch - NUM_START;
         }
         else
         {
-            if(ch >= '0' && ch <= '9')
-                return ch - '0';
-            else if(ch >= ALPHA_START && ch <= ALPHA_END)
-                return ch - ALPHA_START + 10;
+            const char MAX_ALPHA = ALPHA_START + (N - NUM_COUNT);
+
+            if(ch >= NUM_START && ch <= NUM_START+NUM_COUNT)
+                return ch - NUM_START;
+            else if(ch >= ALPHA_START && ch <= MAX_ALPHA)
+                return ch - ALPHA_START + NUM_COUNT;
             else
                 throw raiseInvalid(ch);
         }
@@ -37,13 +46,15 @@ public:
         if(value >= N)
             throw std::invalid_argument("too high: "+std::to_string(value));
 
-        if(N <= 10)
-            return value + '0';
+        if constexpr(N <= NUM_COUNT)
+            return value + NUM_START;
         else
         {
-            if(value < 10)
-                return value + '0';
-            else if(value >= 10 && value < 26)
+            const char MAX_ALPHA = ALPHA_START + (N - NUM_COUNT);
+
+            if(value < NUM_COUNT)
+                return value + NUM_START;
+            else if(value >= NUM_COUNT && value < MAX_ALPHA)
                 return ALPHA_START + (value - 10);
             else
                 throw std::invalid_argument("invalid: "+std::to_string(value));
